e21c3871f99eb046099dd537977f3a644d0a6829
matih ur rehman <matihurrehman@matihs-MacBook-Pro.local>
2026-07-15 06:21:50 +0100

    Initial commit

---
+++ Backend/README.md
@@ -0,0 +1,1 @@
+# Cloud_storage_project
---
+++ Frontend/script.js
@@ -0,0 +1,40 @@
+function togglePassword(){
+
+
+    const password = document.getElementById("password");
+
+
+    if(password.type === "password"){
+
+        password.type = "text";
+
+    }
+
+    else{
+
+        password.type = "password";
+
+    }
+
+
+}
+function togglePassword(){
+
+    let password = document.getElementById("password");
+
+    if(password.type === "password"){
+
+        password.type = "text";
+
+    }
+
+    else{
+
+        password.type = "password";
+
+    }
+
+}
+function DummyData(){
+    return "Dummydata";
+}
